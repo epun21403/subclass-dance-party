6b6d5017c7c4534f0e0ebf827f299fca2bcac047
Evan Pun <evanpun21403@gmail.com>
2023-04-03 17:26:50 -0400

    refactored to Pseudoclassical for Dancer and BlinkyDancer

--- spec/temp1Spec.js
+++ spec/temp1Spec.js
@@ -1,34 +1,35 @@
-describe('blinkyDancer', function() {
+describe('temp1', function() {
 
-  var blinkyDancer, clock;
+
+  var temp1, clock;
   var timeBetweenSteps = 100;
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new temp1(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
-    expect(blinkyDancer.$node).to.be.an.instanceof(jQuery);
+    expect(temp1.$node).to.be.an.instanceof(jQuery);
   });
 
   it('should have a step function that makes its node blink', function() {
-    sinon.spy(blinkyDancer.$node, 'toggle');
+    sinon.spy(temp1.$node, 'toggle');
     blinkyDancer.step();
-    expect(blinkyDancer.$node.toggle.called).to.be.true;
+    expect(temp1.$node.toggle.called).to.be.true;
   });
 
   describe('dance', function() {
     it('should call step at least once per second', function() {
-      sinon.spy(blinkyDancer, 'step');
-      expect(blinkyDancer.step.callCount).to.be.equal(0);
+      sinon.spy(temp1, 'step');
+      expect(temp1.step.callCount).to.be.equal(0);
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
 
-      expect(blinkyDancer.step.callCount).to.be.equal(1);
+      expect(temp1.step.callCount).to.be.equal(1);
 
       clock.tick(timeBetweenSteps);
-      expect(blinkyDancer.step.callCount).to.be.equal(2);
+      expect(temp1.step.callCount).to.be.equal(2);
     });
   });
 });
